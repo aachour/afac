d5d530442f4590c7f0db3569b3e1cb206b05f75c
aachour <achour1990@gmail.com>
2025-09-17 17:51:40 +0300

    push

--- app/Models/Pages.php
+++ app/Models/Pages.php
@@ -9,9 +9,13 @@ class Pages extends Model
     //
     protected $fillable = [
         'name',
+        'name_arabic',
         'meta_title',
+        'meta_title_arabic',
         'meta_description',
+        'meta_description_arabic',
         'meta_keywords',
+        'meta_keywords_arabic',
         'header_color_id',
         'footer_color_id',
         'in_menu',
