1b809e0b47f300d12ada687d874082511d59d755
aachour <achour1990@gmail.com>
2025-09-25 17:10:45 +0300

    push

--- app/Livewire/Sections/SectionForm.php
+++ app/Livewire/Sections/SectionForm.php
@@ -2,10 +2,34 @@
 
 namespace App\Livewire\Sections;
 
+use App\Models\Sections;
+use Livewire\Attributes\On;
 use Livewire\Component;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class SectionForm extends Component
 {
+
+    use AuthorizesRequests; 
+
+    public $editing = false;
+
+    public $section;
+    
+
+    public function mount($id=''){
+
+        if($id==''){
+            $this->authorize('section-create');
+        }
+        else{
+
+            $this->authorize('section-edit');
+            
+        }
+        
+    }
+
     public function render()
     {
         return view('livewire.sections.section-form');
--- resources/views/livewire/sections/section-form.blade.php
+++ resources/views/livewire/sections/section-form.blade.php
@@ -1,3 +1,51 @@
 <div>
-    {{-- Care about people's approval and you will be their prisoner. --}}
+    <div class="row">
+        <div class="col-xl">
+            <form wire:submit="store" class="row g-3">
+
+                <div class="col-12">
+                    
+                    <div class="card">
+                        <div class="card-header d-flex justify-content-between align-items-center">
+                            <h5 class="mb-0">{{ $editing ? "Edit" : "Create" }} Section</h5>
+                            <a href="{{ route('pages') }}" class="btn btn-primary mb-2 text-nowrap">
+                                Pages
+                            </a>
+                        </div>
+                        <div class="card-body">
+
+                            <div class="row">
+
+                                <div class="col-12 col-md-6 mt-2">
+                                    <label class="form-label" for="name">Page Name <span class="text-danger">*</span></label>
+                                    <input
+                                        wire:model="name"
+                                        type="text"
+                                        id="name"
+                                        class="form-control"
+                                        placeholder="Page Name" />
+                                    @error('name') <div class="text-danger">{{ $message }}</div> @enderror
+                                </div>
+
+                            </div>
+
+                        </div>
+                    
+                    </div>
+
+                </div>
+                                
+
+                <div class="col-12 text-end mt-4">
+                    <button type="submit" class="btn btn-primary me-sm-3 me-1">Submit</button>
+                </div>
+
+            </form>
+
+        </div>
+    </div>
+
+    <script>
+       
+    </script>
 </div>
