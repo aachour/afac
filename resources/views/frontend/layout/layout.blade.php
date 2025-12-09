0f1e973f5cda60b361595215b8f8053d7e9270d5
aachour <achour1990@gmail.com>
2025-12-09 17:20:57 +0200

    push

--- resources/views/frontend/layout/layout.blade.php
+++ resources/views/frontend/layout/layout.blade.php
@@ -36,10 +36,83 @@
 
 <body>
 
+    <!--Header-->
+    <div class="header" style="background:{{$headerBgCode}};">
+        <div class="centerContainer">
+            <div class="logo">
+                <img src="{{asset('frontend/images/logo.svg')}}" width="100px" />
+            </div>
+            <div class="topSpacerSmall menu tiny ABCDiatypeMedium">Menu</div>
+            <div class="topSpacerSmall lang rightSpacerBig tiny ABCDiatypeMedium">EN/AR</div>
+            <div class="clear"></div>
+        </div>
+    </div>
+    
+    <!--Page Content-->
     @yield('content')
 
-    <div class="footer">
-        
+    <!--Footer-->
+    <div class="footer" style="background:{{$footerBgCode}};">
+        <div class="centerContainer">
+            <div class="row">
+
+                <div class="col-lg-3 col-12">
+                    <img src="{{asset('frontend/images/logo.svg')}}" width="60%" />
+                </div>
+
+                <div class="col-lg-3 col-12">
+                    <div class="small black ABCDiatypeMedium">About</div>
+                    <div class="topSpacerSmaller">
+                        <a href="#" class="tiny black">Mission & Vision</a>
+                    </div>
+                    <div class="">
+                        <a href="#" class="tiny black">Board of Trustees</a>
+                    </div>
+                    <div class="">
+                        <a href="#" class="tiny black">Team</a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-12">
+                    <div class="small black ABCDiatypeMedium">Get in Touch</div>
+                    <div class="topSpacerSmaller">
+                        <a href="#" class="tiny black">Contact</a>
+                    </div>
+                    <div class="">
+                        <a href="#" class="tiny black">Facebook</a>
+                    </div>
+                    <div class="">
+                        <a href="#" class="tiny black">Insta</a>
+                    </div>
+                    <div class="">
+                        <a href="#" class="tiny black">X</a>
+                    </div>
+                    <div class="">
+                        <a href="#" class="tiny black">Youtube</a>
+                    </div>
+                    <div class="">
+                        <a href="#" class="tiny black">Linkedin</a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-12">
+                    <div class="small black ABCDiatypeMedium">Get Involved</div>
+                    <div class="topSpacerSmaller">
+                        <a href="#" class="tiny black">Support Us</a>
+                    </div>
+                    <div class="">
+                        <a href="#" class="tiny black">Subscribe to Newsletter</a>
+                    </div>
+                    <div class="">
+                        <a href="#" class="tiny black">Work With Us</a>
+                    </div>
+                    <div class="">
+                        <a href="#" class="tiny black">Events</a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     </div>
 
     <script>
