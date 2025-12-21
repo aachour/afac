04494dcf37f15b0d9444701dfffe6cff685d97de
aachour <achour1990@gmail.com>
2025-12-21 15:08:44 +0200

    push

--- app/Helpers/ui.php
+++ app/Helpers/ui.php
@@ -870,12 +870,12 @@
                 foreach($column->accordions as $key=>$accordion){
 
                     $htmlColumn.= '<div class="mb-4">
-                        <div class="accordionTitle small black ABCDiatypeMedium">'.$accordion->title.'</div>
+                        <div class="accordionTitle medium black ABCDiatypeMedium">'.$accordion->title.'</div>
                         <div class="accordionArrow clickable" status="'.($key == '0' ? '1' : '0').'">
                             <img src="'.asset('frontend/images/'.($key == '0' ? 'arrow-up.png' : 'arrow-down.png')).'" width="30px" />
                         </div>
                         <div class="clear"></div>
-                        <div class="accordionText mt-2 tiny black '.($key == '0' ? '' : 'd-none').' ">'.$accordion->text.'</div>
+                        <div class="accordionText mt-2 small black '.($key == '0' ? '' : 'd-none').' ">'.$accordion->text.'</div>
                     </div>';
                 }
 
