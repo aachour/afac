433a9bc086f61f903eb84e0ab637ce1459b26a0c
aachour <achour1990@gmail.com>
2026-01-13 14:21:44 +0200

    push

--- app/Helpers/ui.php
+++ app/Helpers/ui.php
@@ -1108,17 +1108,17 @@
 
                     $htmlColumn.= '<div class="coutdown">
                     
-                        <div class="medium black ABCDiatypeMedium">'.$countdown->title.'</div>
+                        <div class="medium black ABCDiatypeMedium text-center">'.$countdown->title.'</div>
 
                         <div class="row mt-5 align-items-center">
-                            <div class="mt-4 col-12 col-lg-4 text-center text-lg-end">
+                            <div class="mt-4 col-12 col-lg-4 text-center">
                                 <div class="huge black ABCDiatypeMedium">'.$days.'</div>
                                 <div class="big black ABCDiatypeMedium">Day(s)</div>
                             </div>
                             <div class="mt-4 col-12 col-lg-4 text-center">
                                 <a href="'.($countdown->button_link ?? '#').'">'.getEntryBtnShape($countdown->button_value,$countdown->button_value_arabic,$countdown->shape->name,$countdown->shapeHover->name,$countdown->buttonColor->code,$countdown->buttonHoverColor->code,$countdown->buttonBgColor->code,$countdown->buttonHoverBgColor->code).'</a>
                             </div>
-                            <div class="mt-4 col-12 col-lg-4 text-center text-lg-start ">
+                            <div class="mt-4 col-12 col-lg-4 text-center">
                                 <div class="huge black ABCDiatypeMedium">'.@$hours.'</div>
                                 <div class="big black ABCDiatypeMedium">Hour(s)</div>
                             </div>
