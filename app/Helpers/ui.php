d1a4e0a0d4f4e9369cd0415b8309807e32ac61ab
aachour <achour1990@gmail.com>
2026-01-27 16:28:58 +0200

    push

--- app/Helpers/ui.php
+++ app/Helpers/ui.php
@@ -61,11 +61,20 @@
                         else
                         { // Resourses/News/Externals
                             $html.='<div class="col-12 text-center">';
+
+                                $html.='<div class="big black ABCDiatypeMedium text-start">'.getEntryTitle($entry).'</div>';
+
+                                $html.='<div class="mt-2 mb-3 small black ABCDiatypeMedium text-start">';
+                                    if($entry->type_id==6){$html.=date('d M Y',strtotime($entry->resource_date));}
+                                    else if($entry->type_id==7){$html.=date('d M Y',strtotime($entry->news_date));}
+                                $html.='</div>';
+
                                 if($entry->image_featured){
                                     $html.='<img src="'.asset('storage/'.$entry->image_full).'" width="100%" />';
                                 }else{
                                     $html.='<img src="'.asset('frontend/images/default-image-full.png').'" width="100%" />';
                                 }
+
                             $html.='</div>';
 
                         }
