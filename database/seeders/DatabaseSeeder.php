2fd8727bc5591986a1375228b88519f30a6031d4
aachour <achour1990@gmail.com>
2026-02-21 13:55:21 +0200

    push

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -29,6 +29,7 @@ class DatabaseSeeder extends Seeder
         $this->call(ExternalCategoriesSeeder::class);
         $this->call(LogoSeeder::class);
         $this->call(ProgramsSeeder::class);
+        $this->call(ProgramYearsSeeder::class);
         $this->call(EventsSeeder::class);
         $this->call(JurorsSeeder::class);
         $this->call(ProjectsSeeder::class);        
