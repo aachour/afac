75b1d6f953146614b96ef9909dbecea31c79af89
aachour <achour1990@gmail.com>
2026-02-25 14:50:55 +0200

    push

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -10,11 +10,33 @@ class ProjectsController extends Controller
     
     public function projects()
     {
-
             
         //get all filters
 
-        $projects=Entries::WHERE(['type_id'=>'3','published'=>'1'])->get();
+        $projects = Entries::where([
+            'type_id'   => 3,
+            'published' => 1
+        ])->get(['id', 'project_categories_id', 'project_countries_id']);
+
+        $projects_ids = $projects->pluck('id')->toArray();
+
+        $projects_categories_ids = $projects
+            ->pluck('project_categories_id')
+            ->filter()
+            ->flatMap(fn($item) => explode(',', $item))
+            ->unique()
+            ->values()
+            ->toArray();
+
+        $projects_countries_ids = $projects
+            ->pluck('project_countries_id')
+            ->filter()
+            ->flatMap(fn($item) => explode(',', $item))
+            ->unique()
+            ->values()
+            ->toArray();
+
+        dd($projects_ids,$projects_categories_ids,$projects_countries_ids);
 
         return view('frontend.projects');
     }
