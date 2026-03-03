07606f3a8db10e36099cdcd1f2cfc256410c32d7
aachour <achour1990@gmail.com>
2026-03-03 16:19:44 +0200

    push

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -384,7 +384,7 @@ class HomeController extends Controller
                                     $html.='<div class="featured_info">
                                         <div class="title_or_labels" style="'.$title_position.'">
                                             <div class="medium white ABCDiatypeMedium">'.$entry_title.'</div>
-                                            <div class="topSpacerSmall tiny white threeQuartersText">'.mb_substr($entry_text,0,350).'...</div>';
+                                            <div class="topSpacerSmall tiny white threeQuartersText">'.mb_substr($entry_text,0,350).'</div>';
                                         $html.='</div>';
 
                                         if($with_label==1)
