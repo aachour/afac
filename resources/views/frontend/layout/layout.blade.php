8f0a4d933a1045bc24ff87045cd0d8d078446356
aachour <achour1990@gmail.com>
2026-03-19 14:51:09 +0200

    push

--- resources/views/frontend/layout/layout.blade.php
+++ resources/views/frontend/layout/layout.blade.php
@@ -23,7 +23,7 @@
     <link rel="stylesheet" href="{{asset('frontend/css/jquery-ui.css')}}" />
     <link rel="stylesheet" href="{{asset('frontend/css/swiper.css')}}" />
     <link rel="stylesheet" href="{{asset('frontend/css/general.css')}}?v=4" />
-    <link rel="stylesheet" href="{{ asset('frontend/css/style.css') }}?v=9">
+    <link rel="stylesheet" href="{{ asset('frontend/css/style.css') }}?v=10">
     <link rel="stylesheet" href="{{ asset('frontend/css/elements.css') }}?v=8">
     
     <!-- JS -->
