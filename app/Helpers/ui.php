4e06902bd71891631fb4b8c88b454740223556e3
aachour <achour1990@gmail.com>
2026-03-19 16:28:36 +0200

    push

--- app/Helpers/ui.php
+++ app/Helpers/ui.php
@@ -221,7 +221,7 @@ function ViewCollection($collection_id, $language = 'EN')
 
     if ($with_featured == 0 && $show_view_all == 1 && $featured_width != '74.3%') {
         $html .= '<div class="viewAll">
-            <a href="' . $view_all_link . '" class="black tiny ABCDiatypeBlack">' . $view_all_title . ' &nbsp;<img src="' . asset('frontend/images/view-all-btn-en.png') . '" width="9px" style="margin-top:8px;"></a>
+            <a href="' . $view_all_link . '" class="black tiny ABCDiatypeBlack">' . $view_all_title . ' &nbsp;<img src="' . asset('frontend/images/view-all-btn-en.png') . '" width="9px" style="margin-top:10px;"></a>
         </div>';
     }
 
