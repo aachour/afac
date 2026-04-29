6b9f9cd6a9f020e373ee3f78bf6b7f4e152bf188
aachour <achour1990@gmail.com>
2026-04-29 09:00:39 +0300

    push

--- resources/views/frontend/layout/layout.blade.php
+++ resources/views/frontend/layout/layout.blade.php
@@ -24,7 +24,7 @@
     <link rel="stylesheet" href="{{asset('frontend/css/swiper.css')}}" />
     <link rel="stylesheet" href="{{asset('frontend/css/general.css')}}?v=7" />
     <link rel="stylesheet" href="{{ asset('frontend/css/style.css') }}?v=13">
-    <link rel="stylesheet" href="{{ asset('frontend/css/elements.css') }}?v=15">
+    <link rel="stylesheet" href="{{ asset('frontend/css/elements.css') }}?v=16">
     
     @if(app()->getLocale()=='ar')
     <link rel="stylesheet" href="{{asset('frontend/css/arabic.css')}}?v=9" />
