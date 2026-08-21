ed85a6ff2269f807f96840e9e58b72da18fe86fe
Ali Achour <achour1990@gmail.com>
2026-08-21 13:21:04 +0300

    push

--- resources/views/frontend/layout/layout.blade.php
+++ resources/views/frontend/layout/layout.blade.php
@@ -23,7 +23,7 @@
     <link rel="stylesheet" href="{{asset('frontend/css/jquery-ui.css')}}" />
     <link rel="stylesheet" href="{{asset('frontend/css/swiper.css')}}" />
     <link rel="stylesheet" href="{{asset('frontend/css/general.css')}}?v=10" />
-    <link rel="stylesheet" href="{{ asset('frontend/css/style.css') }}?v=34">
+    <link rel="stylesheet" href="{{ asset('frontend/css/style.css') }}?v=35">
     <link rel="stylesheet" href="{{ asset('frontend/css/elements.css') }}?v=29">
     
     @if(app()->getLocale()=='ar')
